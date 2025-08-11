a2e72fd7a42425c8cf4edca8ed021651a003bc5e
gyugusti <gustisari26@gmail.com>
2025-08-11 09:17:53 +0700

    feat(auth): add credentials provider

--- src/@auth/global.d.ts
+++ src/@auth/global.d.ts
@@ -1,11 +1,25 @@
-import { User } from '@auth/user';
+/* eslint-disable prettier/prettier */
+import { DefaultSession } from 'next-auth';
+import { User as DbUser } from '@auth/user';
 
 declare module 'next-auth' {
-	interface Session {
-		accessToken?: string;
-		db: User;
-	}
-	interface JWT {
-		accessToken?: string;
-	}
+        interface Session {
+                accessToken?: string;
+                user: {
+                        id: number;
+                        username: string;
+                        nama_lengkap: string;
+                        role: string;
+                } & DefaultSession['user'];
+                db?: DbUser;
+        }
+        interface JWT {
+                accessToken?: string;
+                user?: {
+                        id: number;
+                        username: string;
+                        nama_lengkap: string;
+                        role: string;
+                };
+        }
 }
