27731393a331951207d8cad031b1e4feac3a5592
gyugusti <gustisari26@gmail.com>
2025-08-11 09:34:15 +0700

    refactor(auth): improve credentials provider

--- src/@auth/global.d.ts
+++ src/@auth/global.d.ts
@@ -1,11 +1,24 @@
-import { User } from '@auth/user';
+import { DefaultSession } from 'next-auth';
+import { User as DbUser } from '@auth/user';
 
 declare module 'next-auth' {
-	interface Session {
-		accessToken?: string;
-		db: User;
-	}
-	interface JWT {
-		accessToken?: string;
-	}
+  interface Session {
+    accessToken?: string;
+    user: {
+      id: number;
+      username: string;
+      nama_lengkap: string;
+      role: string;
+    } & DefaultSession['user'];
+    db?: DbUser;
+  }
+  interface JWT {
+    accessToken?: string;
+    user?: {
+      id: number;
+      username: string;
+      nama_lengkap: string;
+      role: string;
+    };
+  }
 }
